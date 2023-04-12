7b275dd152edef6e29bc66cbb453799db2cd7ce5
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-12 17:39:26 -0400

    feat(app): create principal route for obtain general information of the server application

---
+++ src/app.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Controller({
+	version: VERSION_NEUTRAL,
+})
+export class AppController {
+	constructor(private configService: ConfigService) {}
+
+	@Get()
+	getHello(): string {
+		const port = this.configService.get<number>('app.port');
+		const appUrl = this.configService.get<string>('app.url');
+		const supportEmail = this.configService.get<string>('app.supportEmail');
+		const appEnv = this.configService.get<string>('app.env');
+		const debugMode = this.configService.get<boolean>('app.debugMode');
+
+		const debugEnabled = debugMode ? 'yes' : 'no';
+
+		return (
+			'<h1 align="center">' +
+			(appUrl +
+				':' +
+				port +
+				' | ' +
+				supportEmail +
+				' >> ' +
+				appEnv +
+				' environment in debug mode: ' +
+				debugEnabled) +
+			'</h1>'
+		);
+	}
+}
