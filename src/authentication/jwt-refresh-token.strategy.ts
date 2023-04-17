2765ebb18c2aca36f257cff1e7d458d9271a7607
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-04-17 12:13:10 -0400

    feat(authentication): create jwt refresh token strategy

---
+++ src/authentication/jwt-refresh-token.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../features/users/users.service';
+import { Request } from 'express';
+import { TokenPayload } from './token-payload.interface';
+
+@Injectable()
+export class JwtRefreshTokenStrategy extends PassportStrategy(Strategy, 'jwt-refresh-token') {
+	constructor(private readonly configService: ConfigService, private readonly userService: UsersService) {
+		super({
+			jwtFromRequest: ExtractJwt.fromExtractors([
+				(request: Request) => {
+					return request?.cookies?.Refresh;
+				},
+			]),
+			secretOrKey: configService.get('jwt.refreshTokenSecret'),
+			passReqToCallback: true,
+		});
+	}
+
+	async validate(request: Request, payload: TokenPayload) {
+		const refreshToken = request.cookies?.Refresh;
+		return this.userService.getUserIfRefreshTokenMatches(refreshToken, payload.userId);
+	}
+}
