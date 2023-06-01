3eb88237f4b522cd198f2f46b24533d85934e2df
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-31 22:30:13 -0400

    feat(users): create files relations with user entity

--- src/features/users/entities/user.entity.ts
+++ src/features/users/entities/user.entity.ts
@@ -1,7 +1,11 @@
 import { Exclude } from 'class-transformer';
 import { Role } from 'src/authorization/role.enum';
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Address } from './address.entity';
+// import { DatabaseFile } from 'src/features/database-files/entities/database-file.entity';
+import { PrivateFile } from 'src/features/files/entities/private-file.entity';
+import { LocalFile } from 'src/features/local-files/entities/local-file.entity';
+import { Permission } from 'src/authorization/types/permission.type';
 
 @Entity()
 export class User {
@@ -30,6 +34,14 @@ export class User {
 	@JoinColumn()
 	public address?: Address;
 
+	@Column({
+		type: 'enum',
+		enum: Permission,
+		array: true,
+		default: [],
+	})
+	public permissions?: Permission[];
+
 	@Column()
 	@Exclude()
 	public password: string;
@@ -37,6 +49,35 @@ export class User {
 	@Column({ nullable: true })
 	@Exclude()
 	public currentHashedRefreshToken?: string;
+
+	// TODO: use Amazon S3 to store avatar publicly
+	// @JoinColumn()
+	// @OneToOne(() => PublicFile, {
+	//   eager: true,
+	//   nullable: true,
+	// })
+	// public avatar?: PublicFile;
+
+	// TODO: store file directly to postgres database
+	// @JoinColumn({ name: 'avatarId' })
+	// @OneToOne(() => DatabaseFile, {
+	// 	nullable: true,
+	// })
+	// public avatar?: DatabaseFile;
+
+	// TODO: store file into the sistem file
+	@JoinColumn({ name: 'avatarId' })
+	@OneToOne(() => LocalFile, {
+		nullable: true,
+	})
+	public avatar?: LocalFile;
+
+	// TODO: this field is necessary only for storing files to postgres database
+	@Column({ nullable: true })
+	public avatarId?: number;
+
+	@OneToMany(() => PrivateFile, (file: PrivateFile) => file.owner)
+	public files?: PrivateFile[];
 
 	@Column({ nullable: true })
 	@Exclude()
