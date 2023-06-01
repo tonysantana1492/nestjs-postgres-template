4533bbf8c4852cc608fa993cc6dea9fa6fce2f77
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-31 22:11:21 -0400

    feat(local-files): create local-files module for storing in the system file

---
+++ src/features/local-files/local-files.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Param, ParseIntPipe, Res, StreamableFile } from '@nestjs/common';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { Response } from 'express';
+import { LocalFilesService } from './local-files.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('local-files')
+@ApiTags('local-files')
+export class LocalFilesController {
+	constructor(private readonly localFilesService: LocalFilesService) {}
+
+	@Get(':id')
+	async getDatabaseFileById(@Param('id', ParseIntPipe) id: number, @Res({ passthrough: true }) response: Response) {
+		const file = await this.localFilesService.getFileById(id);
+
+		const stream = createReadStream(join(process.cwd(), file.path));
+
+		response.set({
+			'Content-Disposition': `inline; filename="${file.filename}"`,
+			'Content-Type': file.mimetype,
+		});
+
+		return new StreamableFile(stream);
+	}
+}
