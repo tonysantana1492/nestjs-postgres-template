bbffa3deb24f769079a58843e96d42be901fbfc4
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-05-31 22:13:20 -0400

    feat(database-files): create database-files module for the storing in the postgres database

---
+++ src/features/database-files/database-files.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Param, ParseIntPipe, Res, StreamableFile } from '@nestjs/common';
+import { DatabaseFilesService } from './database-files.service';
+import { Readable } from 'stream';
+import { Response } from 'express';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller('database-files')
+@ApiTags('database-files')
+export class DatabaseFilesController {
+	constructor(private readonly databaseFilesService: DatabaseFilesService) {}
+
+	@Get(':id')
+	async getDatabaseFileById(@Param('id', ParseIntPipe) id: number, @Res({ passthrough: true }) response: Response) {
+		const file = await this.databaseFilesService.getFileById(id);
+
+		const stream = Readable.from(file.data);
+
+		response.set({
+			'Content-Disposition': `inline; filename="${file.filename}"`,
+			'Content-Type': 'image',
+		});
+
+		return new StreamableFile(stream);
+	}
+}
