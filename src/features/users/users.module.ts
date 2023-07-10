be06d1c885f1a70cdefc554a18e2f105cbe709fc
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-10 17:15:44 -0400

    feat(users): add LocalFilesModule, DatabaseFilesModule and FilesModule to imports to use its services

--- src/features/users/users.module.ts
+++ src/features/users/users.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { LocalFilesModule } from '../local-files/local-files.module';
+import { DatabaseFilesModule } from '../database-files/database-files.module';
+import { FilesModule } from '../files/files.module';
 
 @Module({
-	imports: [TypeOrmModule.forFeature([User])],
+	imports: [TypeOrmModule.forFeature([User]), LocalFilesModule, DatabaseFilesModule, FilesModule],
 	controllers: [UsersController],
 	providers: [UsersService],
 	exports: [UsersService],
