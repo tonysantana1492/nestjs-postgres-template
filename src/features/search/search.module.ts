14d600c483ec11ab0504e40f491373af6da97af1
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-10 17:19:26 -0400

    feat(search): create search module to use ElasticSearch

---
+++ src/features/search/search.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ElasticsearchModule } from '@nestjs/elasticsearch';
+import { elasticSearchModuleOptions } from '../../config/elastic-search.config';
+
+@Module({
+	imports: [ConfigModule, ElasticsearchModule.registerAsync(elasticSearchModuleOptions)],
+	exports: [ElasticsearchModule],
+})
+export class SearchModule {}
