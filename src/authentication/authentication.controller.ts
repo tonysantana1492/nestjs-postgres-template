dbe9eda9d9b4cc4001ea08495761fad664281143
Tony Santana Lopez <tonysantana1492@gmail.com>
2023-07-10 16:46:06 -0400

    feat(authentication): add AuthRoles guard to protect routes

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Get, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import { AuthenticationService } from './authentication.service';
 import { RegisterDto } from './dto/register.dto';
-import { User } from 'src/features/users/entities/user.entity';
 import { EmailConfirmationService } from 'src/features/email-confirmation/email-confirmation.service';
 import { UsersService } from 'src/features/users/users.service';
-import { RequestWithUser } from './request-with-user.interface';
+import { RequestWithUser } from './interfaces/request-with-user.interface';
 import { LogInDto } from './dto/login.dto';
-import { LocalAuthenticationGuard } from './local-authentication.guard';
-import { JwtAuthenticationGuard } from './jwt-authentication.guard';
-import { JwtRefreshGuard } from './jwt-refresh.guard';
+import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
+import { LocalAuthenticationGuard } from './guards/local-authentication.guard';
+import { AuthRoles } from 'src/authorization/decorators/role.decorator';
 
 @Controller('authentication')
 @ApiTags('authentication')
@@ -20,29 +19,7 @@ export class AuthenticationController {
 		private readonly emailConfirmationService: EmailConfirmationService,
 	) {}
 
-	@UseGuards(JwtAuthenticationGuard)
-	@Get()
-	authenticate(@Req() request: RequestWithUser) {
-		const user = request.user;
-		return user;
-	}
-
-	@UseGuards(JwtRefreshGuard)
-	@Get('refresh')
-	refresh(@Req() request: RequestWithUser) {
-		const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(request.user.id);
-
-		request.res.setHeader('Set-Cookie', accessTokenCookie);
-		return request.user;
-	}
-
-	@Post('register')
-	async register(@Body() registrationData: RegisterDto): Promise<User> {
-		const user = await this.authenticationService.register(registrationData);
-		await this.emailConfirmationService.sendVerificationLink(registrationData);
-		return user;
-	}
-
+	// TODO: for test propouses
 	@HttpCode(204)
 	@Post('send-email')
 	async sendEmail(@Body() registrationData: RegisterDto): Promise<any> {
@@ -50,10 +27,28 @@ export class AuthenticationController {
 		return;
 	}
 
-	@HttpCode(200)
-	@Get('redis')
-	redis() {
-		return 'mmmmmmm';
+	@AuthRoles()
+	@ApiBearerAuth()
+	@Get('')
+	authenticate(@Req() request: RequestWithUser) {
+		const user = request.user;
+		return user;
+	}
+
+	@UseGuards(JwtRefreshGuard)
+	@ApiBearerAuth()
+	@Get('refresh')
+	refresh(@Req() request: RequestWithUser) {
+		const accessToken = this.authenticationService.getJwtAccessToken(request.user.id);
+
+		return { token: accessToken };
+	}
+
+	@Post('register')
+	async register(@Body() registrationData: RegisterDto) {
+		await this.authenticationService.register(registrationData);
+		await this.emailConfirmationService.sendVerificationLink(registrationData);
+		return;
 	}
 
 	@HttpCode(200)
@@ -62,27 +57,21 @@ export class AuthenticationController {
 	@ApiBody({ type: LogInDto })
 	async login(@Req() request: RequestWithUser) {
 		const { user } = request;
-		const accessTokenCookie = this.authenticationService.getCookieWithJwtAccessToken(user.id);
 
-		const { cookie: refreshTokenCookie, token: refreshToken } =
-			this.authenticationService.getCookieWithJwtRefreshToken(user.id);
-
-		await this.usersService.setCurrentRefreshToken(refreshToken, user.id);
-
-		request.res.setHeader('Set-Cookie', [accessTokenCookie, refreshTokenCookie]);
+		const accessToken = this.authenticationService.getJwtAccessToken(user.id);
 
 		if (user.isTwoFactorAuthenticationEnabled) {
 			return;
 		}
 
-		return user;
+		return { token: accessToken };
 	}
 
-	@UseGuards(JwtAuthenticationGuard)
-	@Post('logout')
 	@HttpCode(200)
+	@AuthRoles()
+	@ApiBearerAuth()
+	@Post('logout')
 	async logOut(@Req() request: RequestWithUser) {
 		await this.usersService.removeRefreshToken(request.user.id);
-		request.res.setHeader('Set-Cookie', this.authenticationService.getCookiesForLogOut());
 	}
 }
